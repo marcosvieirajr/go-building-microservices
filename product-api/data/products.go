3fd51123f4465e1b17a46848ba24ab1d01eacbdb
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-01-27 22:35:17 -0300

    docs: add contents

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -18,13 +18,21 @@ type product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+// Products is a collection of Product
 type Products []*product
 
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
 
+// GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
 }
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -7,25 +7,35 @@ import (
 	"github.com/marcosvieirajr/go-multi-tier-microservices/data"
 )
 
+// Products is a http.Handler
 type products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *products {
 	return &products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and satisfies the http.Handler
+// interface
 func (p *products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		getProducts(rw, r)
 	}
 
 	// catch all
+	// if no method is satisfied return an error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func getProducts(rw http.ResponseWriter, r *http.Request) {
+// getProducts returns the products from the data store
+
+	// fetch the products from the datastore
 	lp := data.GetProducts()
+
+	// serialize the list to JSON
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
