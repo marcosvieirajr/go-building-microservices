3c7c2ae1a8c1cefcb8374768036a081c98d4c535
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-02-11 19:05:00 -0300

    docs: comment main method

--- currency/main.go
+++ currency/main.go
@@ -14,17 +14,26 @@ import (
 func main() {
 	log := hclog.Default()
 
+	// create a new gRPC server, use WithInsecure to allow http connections
 	gs := grpc.NewServer()
+
+	// create an instance of the Currency server
 	cs := server.New(log)
 
+	// register the currency server
 	proto.RegisterCurrencyServer(gs, cs)
+
+	// register the reflection service which allows clients to determine the methods
+	// for this gRPC service
 	reflection.Register(gs)
 
+	// create a TCP socket for inbound server connections
 	l, err := net.Listen("tcp", ":9092")
 	if err != nil {
 		log.Error("unable to listen", "error", err)
 		os.Exit(1)
 	}
 
+	// listen for requests
 	gs.Serve(l)
 }
