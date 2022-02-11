d3890c471b7150f702d24445cf84345bf9975e1a
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-02-11 18:25:26 -0300

    refactor: change imports declarations

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -3,20 +3,21 @@ package server
 import (
 	"context"
 
-	hclog "github.com/hashicorp/go-hclog"
-	"github.com/marcosvieirajr/go-multi-tier-microservices/currency/protos/currency"
+	"github.com/hashicorp/go-hclog"
+	"github.com/marcosvieirajr/go-multi-tier-microservices/currency/proto"
 )
 
 type Currency struct {
 	log hclog.Logger
+	proto.UnimplementedCurrencyServer
 }
 
 func New(l hclog.Logger) *Currency {
-	return &Currency{l}
+	return &Currency{log: l}
 }
 
-func (c *Currency) GetRate(ctx context.Context, rr *currency.RateRequest) (*currency.RateResponse, error) {
+func (c *Currency) GetRate(ctx context.Context, rr *proto.RateRequest) (*proto.RateResponse, error) {
 	c.log.Info("handle RateRequest", "base", rr.GetBase(), "destination", rr.GetDestination())
 
-	return &currency.RateResponse{Rate: 0.5}, nil
+	return &proto.RateResponse{Rate: 0.5}, nil
 }
